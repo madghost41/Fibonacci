47307a951c512601af001f5bb35650203a80ae5f
madghost41 <stephenmicheal97@gmail.com>
2024-01-31 06:22:42 -0600

    first message

--- fibonacci.js
+++ fibonacci.js
@@ -1,5 +1,22 @@
 function fibonacci(num) {
-  return 0;
-}
+let previous = 0
+let current = 1
+let next = 0
 
+if(num === 0){
+  return 0
+} else{
+      
+for (i = 1; num > i; i++){
+  next = previous + current
+     previous = current
+    current = next
+         
+  }
+}
+return total
+}
 module.exports = fibonacci;
+
+
+// F(n) = F(n - 1) + F(n - 2)
